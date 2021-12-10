c6fc1ea5ea0064be74d2891e26573e19e32c07e6
kwkim <83457230+kwkimonline@users.noreply.github.com>
2021-12-11 00:36:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import argparse
 import numpy as np
 
 from config import yaml_config_hook
-from di import di
+from runner import runner
 
 if __name__ == '__main__':
         
-    parser = argparse.ArgumentParser(description="SLIDE + DI")
+    parser = argparse.ArgumentParser(description="SLIDE")
     config = yaml_config_hook("config.yaml")
     for k, v in config.items():
         parser.add_argument(f"--{k}", default=v, type=type(v))
 
     args = parser.parse_args()
 
-    di(args)
+    runner(args)
