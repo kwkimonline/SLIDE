6f268263af2f8f983be6dcfad54b379beff50093
kwkim <83457230+kwkimonline@users.noreply.github.com>
2021-12-11 00:30:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from di import di
 if __name__ == '__main__':
         
     parser = argparse.ArgumentParser(description="SLIDE + DI")
-    config = yaml_config_hook("config_di.yaml")
+    config = yaml_config_hook("config.yaml")
     for k, v in config.items():
         parser.add_argument(f"--{k}", default=v, type=type(v))
 
